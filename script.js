b85b54c4e3f5697a54b61030c53d45f284465490
ptlmeet27 <20124040@nuv.ac.in>
2022-07-12 19:20:43 +0530

    form display visibility

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let library = [];
 
 function Book(title, author, pages, isRead) {
@@ -7,3 +6,23 @@ function Book(title, author, pages, isRead) {
   this.pages = pages;
   this.isRead = isRead;
 }
+
+const formcontainer = document.getElementById("addbookcontainer");
+const closebtn = document.getElementById("close-btn");
+const addbookbtn = document.getElementById("add-book-btn");
+
+formcontainer.style.display = "none";
+
+
+
+
+addbookbtn.addEventListener("click", () => {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
+  formcontainer.style.display = "block";
+});
+
+closebtn.addEventListener("click", () => {
+    formcontainer.style.display = "none";
+})
